67751579a13ca49c96be874cf2f9385f0d33318f
aquiire <aquire@outlook.com>
2020-08-21 15:45:25 +0530

    filename read correction,explicit return ,cache

--- src/main/scala/parquetreader.scala
+++ src/main/scala/parquetreader.scala
@@ -14,25 +14,27 @@ object ParquetReader
 {
   def apply( sc: SparkContext, filename: String, logFile: LogFile ): Graph = {
     val jsonReader = new JsonReader(filename)
-    val verticesFile = jsonReader.getObj("Vertex File").toString
-    val edgesFile = jsonReader.getObj("Edge File").toString
+    val verticesFile = jsonReader.getObj("Vertex File").value.toString
+    val edgesFile = jsonReader.getObj("Edge File").value.toString
 
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val vertices = sqlContext.read.parquet(verticesFile)
+    val vertices:RDD[(Long,(String,Long))] = sqlContext.read.parquet(verticesFile)
     .rdd
     .map {
       case Row( idx: Long, name: String, module: Long )
       => (idx,(name,module))
     }
-	logFile.write(s"Read in vertex information from $verticesFile\n",false)
+    vertices.cache
+	  logFile.write(s"Read in vertex information from $verticesFile\n",false)
 
-    val edges = sqlContext.read.parquet(edgesFile)
+    val edges:RDD[(Long,(Long,Double))] = sqlContext.read.parquet(edgesFile)
     .rdd
     .map {
       case Row( from: Long, to: Long, weight: Double )
       => (from,(to,weight))
     }
-	logFile.write(s"Read in edge information from $edgesFile\n",false)
+    edges.cache
+  	logFile.write(s"Read in edge information from $edgesFile\n",false)
 
     Graph( vertices, edges )
   }
