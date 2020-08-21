bfd1d6976fd37aefa13f1a6e7f0a8cb8ef63a9d7
aquiire <aquire@outlook.com>
2020-08-21 15:47:07 +0530

    import RDD

--- src/main/scala/parquetreader.scala
+++ src/main/scala/parquetreader.scala
@@ -6,7 +6,7 @@
  *****************************************************************************/
 
 import org.apache.spark.SparkContext
-
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SQLContext
 
